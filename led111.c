e29abf4d4d94c98397a8bcc64f9f18c90aa6034e
TIAN-CHENG-RUI <2460152583@qq.com>
2025-10-19 04:31:35 +0800

    流水的

---
+++ led111.c
@@ -0,0 +1,32 @@
+//#include "reg52.h"
+
+
+//sbit led=P2^0;
+
+
+//void main()
+//{
+//	while(1)
+//	{
+//	led=0;
+//	}
+
+//}
+
+//#include <REGX52.H>
+//void Delay500(void); 
+
+
+//void main()
+//{
+//	
+//P2=0xFE;//f1111,e1110
+//	Delay500();
+//	
+//P2=0xFD;
+//		Delay500();
+//	P2=0xFB;
+//	Delay500();
+//}
+
+
